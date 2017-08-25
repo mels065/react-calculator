9c8ad4a8229d0e2e6826c7608fa0c37247aeacc4
Brandon Mellus <mellusbrandon@gmail.com>
2017-08-25 17:52:56 -0400

    Create Calculator component

---
+++ src/js/components/Calculator.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Calculator extends Component {
+  render() {
+    return (
+      <div className="Calculator">
+        {/*Components go here*/}
+      </div>
+    )
+  }
+}
+
+export default Calculator;
